a20ca71784f481835a26452077494e37f50fc97b
username <90975164+adamswebsite@users.noreply.github.com>
2024-05-27 13:56:58 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ibizadesign.github.io/",
+  site: "https://alicantedesign.github.io/",
   integrations: [tailwind(), icon()],
 });
